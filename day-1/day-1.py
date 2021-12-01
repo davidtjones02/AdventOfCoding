c5f9db5c40e744d23d57113ad9232fb9f3655e51
DavidJonesADA <david.jones@ada.ac.uk>
2021-12-01 10:18:01 +0000

    Completed day 1, fixed numbers file

---
+++ day-1/day-1.py
@@ -0,0 +1,14 @@
+# open numbers.txt
+# read each line
+
+with open("numbers.txt", "r") as file:
+    numbers = file.readlines()
+    sum = 0
+     # compare the previous line with the current line
+    for i in range(len(numbers)):
+        # dont compare the first line
+        if i > 0:
+            if int(numbers[i]) > int(numbers[i-1]):
+                sum += 1
+    print(sum)
+    input()
