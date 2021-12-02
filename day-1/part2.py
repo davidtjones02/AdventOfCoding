9a8e6c40450ac5d04e2281866e14d98c1ee6ab4c
DavidJonesADA <david.jones@ada.ac.uk>
2021-12-02 10:30:55 +0000

    Uploaded day 1 from yesterday, created day 2 folder

---
+++ day-1/part2.py
@@ -0,0 +1,27 @@
+with open(
+    "C:/Users/david/Documents/Coding/AdventOfCoding/day-1/numbers.txt", "r"
+) as file:
+    numbers = file.readlines()
+    sum = 607
+    greaterThan = 0
+    group = []
+    for i in range(len(numbers) - 2):
+
+        group = numbers[int(i) : int(i + 3)]
+
+        # convert group to integers
+        group = [int(x) for x in group]
+
+        # print(group)
+
+        # print(len(group))
+
+        tempSum = 0
+        tempSum += group[0] + group[1] + group[2]
+
+        if tempSum > sum:
+            greaterThan += 1
+
+        sum = tempSum
+
+    print(greaterThan)
