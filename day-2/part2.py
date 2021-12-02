f07ec337871c6daad4ce49eca18777decf1a1286
DavidJonesADA <david.jones@ada.ac.uk>
2021-12-02 11:00:56 +0000

    Finished day2, refactoring later

--- day-2/part2.py
+++ day-2/part2.py
@@ -1,19 +1,17 @@
-position = {"horizontal": 0, "depth": 0}
+position = {"horizontal": 0, "aim": 0, "depth": 0}
 
 
 with open("commands.txt", "r") as file:
     # read each line and see if it contains horizontal or depth
     for line in file:
-        print(line)
         if "forward" in line:
             position["horizontal"] += int(line.split()[1])
+            position["depth"] += int(line.split()[1]) * position["aim"]
         elif "up" in line:
-            position["depth"] -= int(line.split()[1])
+            position["aim"] -= int(line.split()[1])
         else:
-            position["depth"] += int(line.split()[1])
+            position["aim"] += int(line.split()[1])
 
 
 # multiply position horiziontal with position depth
 print(position["horizontal"] * position["depth"])
-
-# print(methods)
